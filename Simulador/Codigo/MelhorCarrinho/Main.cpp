cb64bfb27916adc7b715c0f8c8e5749904d464bf
Carlos <carlosh.vier@outlook.com>
2026-05-26 23:02:05 -0300

    Criacao de uma classe Robo, passando algumasfunccoes como metodo, realizando colisao simples

--- Simulador/Codigo/MelhorCarrinho/Main.cpp
+++ Simulador/Codigo/MelhorCarrinho/Main.cpp
@@ -1,46 +1,114 @@
 /*
- * EDUBOT HELLO WORLD EXAMPLE
- * @Author: Maik Basso <maik@maikbasso.com.br>
+ * PROTOTIPO PARA O SIMULADOR
+ * @Author: Carlos Henrique Vier
 */
 
 #include <iostream>
 #include "libs/EdubotLib.hpp"
+#define QT_SONARES 3
+#define QT_BUMPERS 4
+#define QT_BATERIA 3
 
-int main(){
+EdubotLib *edubotLib = new EdubotLib();
 
-	EdubotLib *edubotLib = new EdubotLib();
+class Robo {
+    public:
+        struct posicaoRobo {
+            float x;
+            float y;
+            float theta;
+        }posicao;
 
-	//try to connect on robot
+        struct infoDados {
+        	  int bumpers[QT_BUMPERS];
+        	  int colidindo;
+            float sonares[QT_SONARES];
+            float encoderDireito;
+            float encoderEsquerdo;
+            float celulasBateria[QT_BATERIA];
+        }dados;
+
+        void movimentacao();
+        void movimentaQuadrados(int quantLados);
+        int colisao();
+};
+
+int Robo::colisao(){
+    for(int x = 0; x < QT_BUMPERS; x++){
+        // Correção: mudado de 'i' para 'x' para bater com a variável do loop
+        this->dados.bumpers[x] = edubotLib->getBumper(x); 
+        
+        if(this->dados.bumpers[x] == true){
+            return true; // Retorna verdadeiro se qualquer bumper bateu
+        }
+    }
+    return false; // Correção: Faltava um retorno padrão caso não colida com nada
+}
+
+void Robo::movimentaQuadrados(int quantLados){
+    
+    for(int x = 1; x <= quantLados; x++){ // Correção: alterado para '<=' para rodar a quantidade exata de lados
+        for(int y = 0; y < 4; y++){
+            
+            // Atualiza o estado de colisão na struct do robô
+            this->dados.colidindo = this->colisao();
+            
+            if(!this->colisao()) {
+                edubotLib->move(0.9);        
+                edubotLib->sleepMilliseconds(500 * x);
+        
+                edubotLib->rotate(90);
+                edubotLib->sleepMilliseconds(2000);
+            } else {
+                // Se colidir, para o robô por segurança
+                edubotLib->neutral();
+                edubotLib->sleepMilliseconds(2000);
+                std::cout << "Colisao detectada!\n Movimento interrompido." << std::endl;
+                return; // Sai da função
+            }
+        }
+    }
+}
+
+int main(void){
+	//instancia objeto dentro da main e nomeia;
+	Robo roboAtual;
+	//Tenta conectar Robo
 	if(edubotLib->connect()){
 
 		// shows edubot sensors
-		int showSensorsTimes = 10;
+		int showSensorsTimes = 5;
 		while (showSensorsTimes > 0) {
+			
+			edubotLib->sleepMilliseconds(500);
 
-			// Waits for two seconds
-			edubotLib->sleepMilliseconds(250);
-
-			// sonars
+			// Colhe info dos Sonares - For para colher a info dos 7 sonares em um loop, e retorna a medida em metros;
 			for (int i=0; i<7; i++) {
-				std::cout << "S" << i << ": " << edubotLib->getSonar(i) << "m, ";
+				std::cout << "Sonar " << i << ": " << edubotLib->getSonar(i) << "m, ";
 			}
-
-			// bumpers
+			std::cout << "\n";
+			// Colhe info dos Bumpers - sensores ativados na batida - resposta booleana (true/false)
 			for (int i=0; i<4; i++) {
-				std::cout << "B" << i << ": " << (edubotLib->getBumper(i) == true? "true":"false") << ", ";
+				std::cout << "Bumpers" << i << ": " << (edubotLib->getBumper(i) == true? "true":"false") << ", ";
 			}
-
-			std::cout << "leftcount: " << edubotLib->getEncoderCountLeft() << ", ";
-			std::cout << "rightcount: " << edubotLib->getEncoderCountRight() << ", ";
+			std::cout << "\n";
+			//Colhe info dos Encoders
+			std::cout << "Encoder lado esquerdo: " << edubotLib->getEncoderCountLeft() << ", ";
+			std::cout << "Encoder lado direito: " << edubotLib->getEncoderCountRight() << ", ";
 			std::cout << "dt(looptime): " << edubotLib->getEncoderCountDT() << ", ";
-			
-			std::cout << "x: " << edubotLib->getX() << ", ";
-			std::cout << "y: " << edubotLib->getY() << ", ";
-			std::cout << "theta: " << edubotLib->getTheta() << ", ";
-			
-			std::cout << "battCell0: " << edubotLib->getBatteryCellVoltage(0) << ", ";
-			std::cout << "battCell1: " << edubotLib->getBatteryCellVoltage(1) << ", ";
-			std::cout << "battCell2: " << edubotLib->getBatteryCellVoltage(2);
+			std::cout << "\n";
+			//Medida caluclada do eixo X,Y e do Theta para localização
+			std::cout << "Cordenada X: " << edubotLib->getX() << ", ";
+			std::cout << "Cordenada Y: " << edubotLib->getY() << ", ";
+			std::cout << "Rotacao Theta: " << edubotLib->getTheta() << ", ";
+			std::cout << "\n";
+
+			//Colhe as informações corelacionadas a 3 celulas de materia
+			for (int i=0; i<3; i++){
+			std::cout << "Celula de Bateria " << i << ": "<< edubotLib->getBatteryCellVoltage(i) << "V, ";
+			}
+			std::cout << "\n";
+			std::cout << "\n";
 
 			// line break
 			std::cout << std::endl;
@@ -48,50 +116,15 @@ int main(){
 			showSensorsTimes--;
 
 		}
-
-		/*
-		 * Function move
-		 * Parameter velocity (-1.0, 1.0)
-		 * Positive values move forward, negative values move backward
-		*/
-		edubotLib->move(0.3);
-		// Waits for two seconds for processes to reflect on the robot
-		edubotLib->sleepMilliseconds(2000);
-
-		/*
-		 * Function rotate
-		 * Parameter angle (-180, 180)
-		 * Positive values rotate right, negative values rotate left
-		*/
-		edubotLib->rotate(90);
-		// Waits for two seconds for processes to reflect on the robot
-		edubotLib->sleepMilliseconds(2000);
-
-		/*
-		 * Function neutral
-		 * put the motors in the neutral state
-		*/
-		edubotLib->neutral();
-		// Waits for two seconds for processes to reflect on the robot
-		edubotLib->sleepMilliseconds(2000);
-
-		/*
-		 * Function stop
-		 * Stop the motors / break
-		*/
-		edubotLib->stop();
-		// Waits for two seconds for processes to reflect on the robot
-		edubotLib->sleepMilliseconds(2000);
-
-		/*
-		 * Function disconnect
-		 * disconnect from robot
-		 * Important to user EVER
-		*/
+		//chamando metodo(função) - movimentaQuadrados vou colocar 4;
+		roboAtual.movimentaQuadrados(4);
+		
+		//Disconecta Robo
 		edubotLib->disconnect();
 	}
 	else{
-		std::cout << "Could not connect on robot!" << std::endl;
+		//mensagem de erro
+		std::cout << "Nao foi possivel se conectar ao robo!" << std::endl;
 	}
 
 	return 0;
